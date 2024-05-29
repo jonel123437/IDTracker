8681bccad1a6febe80cc6f0d62abff8d17578fba
NeilUnabia <152669641+NeilUnabia@users.noreply.github.com>
2024-05-29 22:29:59 +0800

    (Unfinish) Update dashboard.js [dropdown script]

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,1 +1,16 @@
+var dropdown = document.querySelector('.dropdown-logout');
+var isOpen = false;
 
+document.getElementById('dropdown').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevents the default behavior of the link
+    
+    if (isOpen) {
+        dropdown.style.display = "none"; 
+        document.getElementById('dropdown1').innerHTML = " ▼"; 
+    } else {
+        dropdown.style.display = "block"; 
+        document.getElementById('dropdown1').innerHTML = " ▲"; 
+    }
+    
+    isOpen = !isOpen; 
+});
