5dcfed0c1495f183448011f5520194ac10b7af59
NeilUnabia <152669641+NeilUnabia@users.noreply.github.com>
2024-05-29 21:55:58 +0800

    (Unfinished) Update dashboard.php [added div class]

--- View/dashboard.php
+++ View/dashboard.php
@@ -10,12 +10,19 @@
     
     ?>
     <header>
-        <div class="header_container">
-            
+            <div class="header_container">
+                <h1 id="dashboard_title" style="cursor: pointer; margin-left: 1rem;">ID Tracker</h1>
+                <div class="header_container1">
+                    <ul class="ul_container">
+                        <li><a href="" id="dropdown"><?= $fullNameProfile ?><a href="" id="dropdown1"> ▼</a></a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <div class="dropdown-logout">
+            <a href="?page=profile">Profile</a>
+            <br>
+            <a href="#" onclick="confirmLogout()">Logout</a>
         </div>
-    </header>
-    <div class="dropdown-logout">
-
-    </div>
-</body>
+    </body>
 </html>
