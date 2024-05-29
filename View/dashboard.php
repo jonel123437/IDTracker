7c529c7ef4c5ab54aabb82df1309a9ee7e8b5a8a
NeilUnabia <152669641+NeilUnabia@users.noreply.github.com>
2024-05-29 17:57:24 +0800

    Update dashboard.php (Unfinished)
    dashboard idea

---
+++ View/dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>idTracker | Dashboard</title>
+</head>
+<body>
+    <?php
+    
+    ?>
+    <header>
+        <div class="header_container">
+            
+        </div>
+    </header>
+    <div class="dropdown-logout">
+
+    </div>
+</body>
+</html>
