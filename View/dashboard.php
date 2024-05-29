140fb2bf8ec804a8cd3e07b4e1b56237859f168c
NeilUnabia <152669641+NeilUnabia@users.noreply.github.com>
2024-05-29 22:21:16 +0800

    (Unfinish) Update dashboard.php [added link css and js script]

--- View/dashboard.php
+++ View/dashboard.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>idTracker | Dashboard</title>
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <link rel="stylesheet" href="./assets/css/dashboard.css">
 </head>
 <body>
     <?php
@@ -25,4 +27,5 @@
             <a href="#" onclick="confirmLogout()">Logout</a>
         </div>
     </body>
+    <script src="assets/js/dashboard.js"></script>
 </html>
