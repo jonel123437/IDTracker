151754f229dc90086cf82a5df890fa6a68eb869a
Candilada <152843047+Candilada@users.noreply.github.com>
2024-05-30 12:16:22 +0800

    Update loginModel.php
    1st commit for login

--- Model/loginModel.php
+++ Model/loginModel.php
@@ -1,1 +1,19 @@
+<?php
+    include "database.php";
 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        
+        $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+        $result = $conn->query($sql);
+
+        if($result->num_rows > 0) {
+            // User found, retrieve the full name
+            $row = $result->fetch_assoc();
+            $fullNameProfile = $row['full_name'];
+            $id_noProfile = $row["id_no"];
+            // Start the session
+            session_start();
+            
+          
