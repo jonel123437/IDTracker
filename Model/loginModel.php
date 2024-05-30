213a91ca33545ef84cf5160a1324a95f7f8087cd
Candilada <152843047+Candilada@users.noreply.github.com>
2024-05-30 12:21:31 +0800

    Update loginModel.php
    2nd commit

--- Model/loginModel.php
+++ Model/loginModel.php
@@ -16,4 +16,27 @@
             // Start the session
             session_start();
             
-          
+            // Store the full name in a session variable
+            $_SESSION['full_name'] = $fullNameProfile;
+            $_SESSION["id_no"] = $id_noProfile;
+
+            // Redirect to the dashboard
+            header("Location: ?page=dashboard");
+        } else {
+        }
+    }
+
+    session_start(); // Start the session to access session variables
+
+    // Check if the full name is set in the session
+    if(isset($_SESSION['full_name'])) {
+        $fullNameProfile = $_SESSION['full_name'];
+    } else {
+        $fullNameProfile = "Guest"; // Default value if full name is not set
+    }
+    if(isset($_SESSION['id_no'])) {
+        $id_noProfile = $_SESSION['id_no'];
+    } else {
+        $id_noProfile = "N/A"; // Default value if ID number is not set
+    }
+?>
