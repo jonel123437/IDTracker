b2f110f8283df926eb7be78b60ae3dcce3e8bd12
NeilUnabia <152669641+NeilUnabia@users.noreply.github.com>
2024-05-30 17:02:49 +0800

    Update dashboard.php (added greetings div class and model link)

--- View/dashboard.php
+++ View/dashboard.php
@@ -1,17 +1,18 @@
-<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>idTracker | Dashboard</title>
-    <link rel="stylesheet" href="./assets/css/styles.css">
-    <link rel="stylesheet" href="./assets/css/dashboard.css">
-</head>
-<body>
-    <?php
-    
-    ?>
-    <header>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>idTracker | Dashboard</title>
+        <link rel="stylesheet" href="./assets/css/styles.css">
+        <link rel="stylesheet" href="./assets/css/home.css">
+        <link rel="stylesheet" href="./assets/css/dashboard.css">
+    </head>
+    <body>
+        <?php
+            include "Model/search.php";
+            include "Model/login.php";
+        ?>
+        <header>
             <div class="header_container">
                 <h1 id="dashboard_title" style="cursor: pointer; margin-left: 1rem;">ID Tracker</h1>
                 <div class="header_container1">
@@ -25,6 +26,15 @@
             <a href="?page=profile">Profile</a>
             <br>
             <a href="#" onclick="confirmLogout()">Logout</a>
+        </div>
+        <div id="greetings" style="position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%);">
+            <h1>WELCOME TO DASHBOARD</h1>
+            <form action="?page=search" method="post">
+                <div>
+                    <input type="text" placeholder="Search ID" name="id_no">
+                    <button type="submit" id="search_id">></button>
+                </div>
+            </form>
         </div>
     </body>
     <script src="assets/js/dashboard.js"></script>
