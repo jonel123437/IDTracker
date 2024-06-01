70645c60d8ff7571020ab6111cc44498467bdac8
Candilada <152843047+Candilada@users.noreply.github.com>
2024-06-01 11:04:18 +0800

    Delete IDTracker directory

--- IDTracker/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    require "Config/_config.php";
-?>
--- IDTracker/uploads.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE uploads (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    file VARCHAR(255),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
