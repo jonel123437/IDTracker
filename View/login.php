a5decacec76ee4ebad6ea2b2f99afdc568921fb6
Candilada <152843047+Candilada@users.noreply.github.com>
2024-06-01 11:32:07 +0800

    Update login.php

--- View/login.php
+++ View/login.php
@@ -24,6 +24,9 @@
         <div class="container <?php if($error) echo 'error'; ?>">
             <form action="?page=login" method="post">
                 <h1 style="margin-bottom: 2rem;">LOGIN</h1>
+                <?php if($error): ?>
+                    <p id="exist_id" style="color: red;">Invalid email or password</p>
+                <?php endif; ?>
                 <div class="input_container">
                     <input id="email" type="email" placeholder="Email" name="email">
                 </div>
