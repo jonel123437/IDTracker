dc1a3748af141711c77d0d56aa107b2a7a79faa1
Dedempogi <150107101+Dedempogi@users.noreply.github.com>
2024-06-01 11:33:09 +0800

    Update registerModel.php

--- Model/registerModel.php
+++ Model/registerModel.php
@@ -1,5 +1,8 @@
 <?php
-include "database.php";
+    include "database.php";
+
+    $success = false; 
+    $error = false; 
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         $full_name = $_POST["full_name"];
@@ -7,13 +10,21 @@ include "database.php";
         $email = $_POST["email"];
         $password = $_POST["password"];
 
-        // Hash the password
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $check_query = "SELECT * FROM users WHERE id_no = '$id_no'";
+        $check_result = $conn->query($check_query);
 
-        $sql = "INSERT INTO users(full_name, id_no, email, password) VALUES ('$full_name', '$id_no', '$email', '$hashed_password')";
-
-        if($conn->query($sql) === TRUE) {
+        if($check_result->num_rows > 0) {
+            $error = true;
         } else {
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+            $sql = "INSERT INTO users(full_name, id_no, email, password) VALUES ('$full_name', '$id_no', '$email', '$hashed_password')";
+
+            if($conn->query($sql) === TRUE) {
+                $success = true;
+            } else {
+                $error = true; 
+            }
         }
     }
 ?>
